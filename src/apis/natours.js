dd9b84019c21683a8cae994f732a30db3383a3fe
Mayur Panchal <mayurpanchal002@gmail.com>
2020-05-09 19:19:00 +0530

    added env base api url support

--- src/apis/natours.js
+++ src/apis/natours.js
@@ -1,3 +1,7 @@
 import Axios from 'axios';
 
-export default Axios.create({ baseURL: 'http://127.0.0.1:8000/api/v1' });
+export default Axios.create({
+	baseURL:
+		`${process.env.REACT_APP_API_BASE_URL}/api/v1` ||
+		'http://127.0.0.1:8000/api/v1',
+});
