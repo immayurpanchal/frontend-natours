c3d01f8013bbd68e09ecac628f37d45d2a43f4b4
Mayur Panchal <mayurpanchal002@gmail.com>
2020-05-24 20:23:49 +0530

    loader added in my tours

--- src/components/MyTours/index.js
+++ src/components/MyTours/index.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCurrentUserBookings } from '../../modules/booking/booking.action';
 import TourCard from '../TourCard';
+import Loading from '../Loading';
 
 const MyTours = () => {
 	const dispatch = useDispatch();
@@ -15,13 +16,14 @@ const MyTours = () => {
 	}, []);
 
 	return (
-		<div className='main'>
+		<main className='main'>
+			{!bookings.length && <Loading width='100px' height='100px' />}
 			<div className='card-container'>
 				{bookings.map((booking) => (
 					<TourCard key={booking.id} tour={booking.tour} />
 				))}
 			</div>
-		</div>
+		</main>
 	);
 };
 
