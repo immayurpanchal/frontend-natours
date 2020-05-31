cd53370918988bb8d1f0e98cf213b9be4956f858
Mayur Panchal <mayurpanchal002@gmail.com>
2020-05-31 09:55:05 +0530

    default api points to prod

--- src/apis/natours.js
+++ src/apis/natours.js
@@ -1,15 +1,14 @@
-import Axios from 'axios';
-import Cookies from 'universal-cookie';
+import Axios from "axios";
+import Cookies from "universal-cookie";
 
 const cookie = new Cookies();
-const token = cookie.get('jwt');
+const token = cookie.get("jwt");
 
 export default Axios.create({
-	baseURL:
-		`${process.env.REACT_APP_API_BASE_URL}/api/v1` ||
-		'https://backend-natours.herokuapp.com/api/v1' ||
-		'http://127.0.0.1:8000/api/v1',
-	headers: {
-		Authorization: `Bearer ${token}`,
-	},
+  baseURL: process.env.REACT_APP_API_BASE_URL
+    ? `${process.env.REACT_APP_API_BASE_URL}/api/v1`
+    : "https://backend-natours.herokuapp.com/api/v1",
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
