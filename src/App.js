92042e925a8102aaf1120a5e497d4c0951bffbd0
Mayur Panchal <mayurpanchal002@gmail.com>
2020-06-05 12:11:11 +0530

    Router replaced with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import { I18nextProvider } from 'react-i18next';
 import LazyLoad from 'react-lazyload';
-import { Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Loading from './components/Loading';
 import Toaster from './components/Toaster';
+import i18n from './i18n-browser';
 import history from './modules/history';
 import { getRoutes } from './routes';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n-browser';
-import Loading from './components/Loading';
 
 const App = (props) => {
 	const { prefix } = props;
 	return (
 		<React.Suspense fallback={Loading}>
 			<I18nextProvider i18n={i18n} initialLanguage={'en'}>
-				<Router history={history}>
+				<BrowserRouter history={history}>
 					<Toaster />
 					<Header />
 					<Switch>
@@ -26,7 +26,7 @@ const App = (props) => {
 					<LazyLoad height='200' once>
 						<Footer />
 					</LazyLoad>
-				</Router>
+				</BrowserRouter>
 			</I18nextProvider>
 		</React.Suspense>
 	);
